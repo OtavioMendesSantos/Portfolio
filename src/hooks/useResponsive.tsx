29199e9cfa9fadd4f95ecf50e7f9c2e6b6dce3d2
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-08-21 21:25:56 -0300

    :sparkles: feat: useResponsive

---
+++ src/hooks/useResponsive.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useMemo, useState } from 'react'
+
+const useResponsive = (): { isMobile: boolean; isDesktop: boolean } => {
+    const [isMobile, setIsMobile] = useState(false)
+    const [isDesktop, setIsDesktop] = useState(false)
+
+    function match(query: string): boolean {
+        return window.matchMedia(query).matches
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize)
+        function handleResize() {
+            setIsMobile(match('(max-width: 600px)'))
+            setIsDesktop(match('(min-width: 600px)'))
+        }
+        handleResize()
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+    return {
+        isMobile,
+        isDesktop,
+    }
+}
+
+export default useResponsive
